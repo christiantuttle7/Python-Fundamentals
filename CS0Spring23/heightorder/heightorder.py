ebce5936336dc5e01fd40acd94190310f47c70aa
Corin Chepko <cchepko2@coloradomesa.edu>
2023-03-10 16:17:12 -0700

    sefg

--- CS0Spring23/heightorder/heightorder.py
+++ CS0Spring23/heightorder/heightorder.py
@@ -9,30 +9,26 @@
 def main():
     n_line = int(input())
 
-    for line in range(n_line):
-        
+    for i in range(n_line):
         in_list = list(map(int, input().split()))
-        k = in_list[0]
+        k = in_list[0] #k is the set number
         order_list = []
-        order_list.append(in_list[1])
-        students = in_list[2::]
+        order_list.append(in_list[1]) # Add the first student to list
+        students = in_list[2::] # list of the rest of student heights
 
         steps=0
-        for stud in students:
-            added=False
+        for stud in students: # For every student
+            added = False
             index=0
-            for s in order_list:
+            for s in order_list: # Find where student should be in line
                 if(stud < s):
                     steps = steps + len(order_list[index::])
-                    order_list.insert(index, stud)
+                    order_list.insert(index, stud) # Add student to ordered list
                     added = True
                     break
                 index += 1
-            if(not added):
+            if( not added):
                 order_list.append(stud)
-
-        
-        #print(order_list)
         print(f"{k} {steps}")
 
 if __name__ == "__main__":
