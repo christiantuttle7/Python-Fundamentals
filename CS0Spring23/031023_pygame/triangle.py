3b044774ce96db7356f526e870506c0f3f456d3f
Corin Chepko <cchepko2@coloradomesa.edu>
2023-03-14 16:57:01 -0600

    sdg

--- CS0Spring23/031023_pygame/triangle.py
+++ CS0Spring23/031023_pygame/triangle.py
@@ -1,15 +1,28 @@
 # pick a point
 # randomly choose a vertix of the triangle
+# calculate the point halfway between the starting point and the guess
 # plot a point halfway between the triangle vertix and the original position
 # from the new position, repeat
 
 import pygame
 import random
 
+def find_mid_point(p1, p2):
+    x_mid = int((p1[0]+p2[0])/2)
+    y_mid = int((p1[1]+p2[1])/2)
+
+    return (x_mid, y_mid)
+
+
 pygame.init()
-width = 1000
-height = 600
-iterations = 10
+width = 1600
+height = 900
+iterations = 10000
+
+triangle_point = [(0, height/3), (width/2, 0), (width-1, height/3), (0, height*2/3), (width/2,height-1), (width-1, height*2/3), (width/2, height/2)]
+
+#xy = (random.randint(0,width-1), random.randint(0,height-1))
+xy = triangle_point[0]
 
 window = pygame.display.set_mode((width, height))
 run = True
@@ -20,10 +33,21 @@ while run:
         if event.type == pygame.KEYUP:
             if(event.key == pygame.K_ESCAPE):
                 run = False
-
+    
     for i in range(iterations):
-        color = (0,255,0)
-        window.set_at((100, 100), color)
+        guess_point = random.choice(triangle_point)
+        mid_point = find_mid_point(xy, guess_point)
+
+        #red = random.randint(0,255)
+        red = mid_point[0]/width * 255
+        green = mid_point[1]/height * 255
+
+        b_scale = (255/(height+width))
+        blue = (mid_point[0]+mid_point[1])*b_scale
+
+        color = (red,green,255-blue)
+        window.set_at(mid_point, color) #Plot a pixel at (mid_point[0],mid_point[1])
+        xy = mid_point
     
     pygame.display.flip()
 
