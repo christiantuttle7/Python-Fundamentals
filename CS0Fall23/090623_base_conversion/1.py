094267811407f8405d602f05a561c7fd84eada83
Corin Chepko <cchepko2@coloradomesa.edu>
2023-09-07 11:50:53 -0600

    commit

---
+++ CS0Fall23/090623_base_conversion/1.py
@@ -0,0 +1,26 @@
+'''
+Corin Chepko
+090623
+Sample code converting between bases
+'''
+
+number = 7
+
+print(number)
+print(hex(number))
+print(bin(number))
+print()
+print(~number)
+print(hex(~number))
+print(bin(~number))
+
+print()
+print(number << 2)
+print(number >> 1)
+
+print()
+#number = 0b0110
+print(~0b10000000000000000000000000000000)
+
+
+
