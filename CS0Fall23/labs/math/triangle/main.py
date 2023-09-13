455729080329b053130b23874113f41199154d09
Corin Chepko <cchepko2@coloradomesa.edu>
2023-09-13 10:41:57 -0600

    nb

---
+++ CS0Fall23/labs/math/triangle/main.py
@@ -0,0 +1,31 @@
+"""
+Math and Variables Lab
+By: FIXME
+CSCI 110 Lab
+Date: FIXME
+ 
+Read and solve: Add Two Numbers - https://open.kattis.com/problems/triarea 
+ 
+Algorithm steps:
+  1. Read data as a line
+  2. Split the line into two numbers
+  3. Calculate the area
+  4. print the result
+"""
+ 
+def main():
+  """Main function that solves the problem
+  """
+
+  # FIXME input data into varible "line"
+  line =
+  # split the data into two numbers
+  h, b = line.split()
+  # check to see if the data is split correctly
+  print(f'{a=}, {b=}') # FIXME 1: comment/remove this line before submitting to Kattis
+  # FIXME 2: convert string h into float
+  # FIXME 3: convert string b into float
+  # FIXME 4: Calculate the area and store into varible called "area"
+  # FIXME 5: print the area as shown in the sample output
+
+main() # call main function
